dc055f99b442552149ce38982c65c3841148f22e
James Brooks <jhbrooks@uchicago.edu>
2016-03-04 10:09:37 -0500

    Add User model with validations and secure password

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304034541) do
+ActiveRecord::Schema.define(version: 20160304143517) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
 
 end
